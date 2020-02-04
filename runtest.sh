357eb55dcb2ef138094425ff5a79f9279ab1d367
xstater <a166224@163.com>
2020-02-05 01:25:08 +0800

    change ' to "

--- runtest.sh
+++ runtest.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+DIR_SRC="./src"
+DIR_SRC_RAM=${DIR_SRC}"/ram"
+
+DIR_OUTPUT="build"
+
 CC=iverilog
 CC_ARGS=-Wall
 
@@ -8,11 +13,6 @@ SIM_ARGS=
 
 WV=gtkwave
 WV_ARGS=
-
-DIR_SRC='./src'
-DIR_SRC_RAM=${DIR_SRC}'/ram'
-
-DIR_OUTPUT='build'
 
 if [[ ! -d ${DIR_SRC} ]]; then
     echo "${DIR_SRC} directory is not exist!"
@@ -26,30 +26,30 @@ if [[ $1 == "ram" ]]; then
         exit 0
     fi
 
-    dir=${DIR_SRC_RAM}'/'${DIR_OUTPUT}
+    dir=${DIR_SRC_RAM}"/"${DIR_OUTPUT}
 
     if [[ ! -d ${dir} ]]; then
         mkdir ${dir}
     fi
-    ${CC} ${CC_ARGS} -s test -o ${dir}'/test.out' ${DIR_SRC_RAM}'/test.v' ${DIR_SRC_RAM}'/ram.v'
+    ${CC} ${CC_ARGS} -s test -o ${dir}"/test.out" ${DIR_SRC_RAM}"/test.v" ${DIR_SRC_RAM}"/ram.v"
     echo "Done"
 
     echo "Start Simulation..."
-    ${SIM} ${SIM_ARGS} ${dir}'/test.out'
+    ${SIM} ${SIM_ARGS} ${dir}"/test.out"
     echo "Done"
     if [[ -f test.vcd ]]; then
-        mv ./test.vcd ${dir}'/test.vcd' #move vcd file
-        ${WV} ${WV_ARGS} ${dir}'/test.vcd'
+        mv ./test.vcd ${dir}"/test.vcd" #move vcd file
+        ${WV} ${WV_ARGS} ${dir}"/test.vcd"
     fi
     exit 0
 fi
 
 if [[ $1 == "clear" ]]; then
     for module in `ls ${DIR_SRC}`; do
-        dir=${DIR_SRC}'/'${module}
+        dir=${DIR_SRC}"/"${module}
         if [[ -d ${dir} ]]; then
-            if [[ -d ${dir}'/build' ]]; then
-                rm -rf ${dir}'/build'
+            if [[ -d ${dir}"/build" ]]; then
+                rm -rf ${dir}"/build"
             fi
         fi
     done
